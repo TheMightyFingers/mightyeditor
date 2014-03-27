b0e0a4755bcf307b609c56af41c28103aad0b7e5
Kaspars Stauzs <stauzs@gmail.com>
2014-03-27 17:59:27 +0200

    new project tmp fix

--- client/MT/core/Project.js
+++ client/MT/core/Project.js
@@ -58,7 +58,7 @@ MT.extend("core.BasicPlugin")(
 			var that = this;
 			
 			var b = ui.topPanel.addButton("NEW", null, function(){
-				window.location = window.location.toString().split("#").join("");
+				window.location = window.location.toString().split("#")[0];
 			});
 			b.width = 80;
 			
