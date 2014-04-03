d347ed47d5e6adf874cebfa8282a957d47795a54
Kaspars Stauzs <stauzs@gmail.com>
2014-04-03 10:58:10 +0300

    init project on soclet open

--- client/js/main.js
+++ client/js/main.js
@@ -12,9 +12,11 @@ MT.onReady(main);
 function main(){
 	var socket = new MT.Socket();
 	var ui = window.ui = new MT.ui.UIController();
-	new MT.core.Project(ui, socket);
-	socket.on("open", function(){
-		console.log("connection opened");
-		
+	
+	socket.on("core", function(type){
+		if(type == "open"){
+			console.log("connection opened");
+			new MT.core.Project(ui, socket);
+		}
 	});
 }
