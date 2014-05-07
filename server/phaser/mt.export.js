6364e7267cd930a586576ae61ba9a3f0bff2bea6
Kaspars Stauzs <stauzs@gmail.com>
2014-05-07 12:33:56 +0300

    export tool

--- server/phaser/mt.export.js
+++ server/phaser/mt.export.js
@@ -22,6 +22,10 @@ global.mt = {
 		this._loadObjects(this.data.objects.contents, this.objects, "");
 	},
 	
+	getAssetPath: function(asset){
+		return this.assetsPath + asset.fullPath;
+	},
+ 
 	_loadAssets: function(data, container, path){
 		path = path !== "" ? path+"." : path;
 		
@@ -42,18 +46,21 @@ global.mt = {
 	},
  
 	_addAsset: function(asset, container, path){
+		var that = this;
 		
 		if(asset.width != asset.frameWidth || asset.height != asset.frameHeight){
-			game.load.spritesheet(asset.path, this.assetsPath + asset.path, asset.frameWidth, asset.frameHeight, asset.frameMax, asset.margin, asset.spacing);
+			this.game.load.spritesheet(asset.key, this.assetsPath + asset.fullPath, asset.frameWidth, asset.frameHeight, asset.frameMax, asset.margin, asset.spacing);
 		}
 		else{
-			game.load.image(asset.path, this.assetsPath + asset.path);
+			this.game.load.image(asset.key, this.assetsPath + asset.fullPath);
 		}
+		
 		
 		Object.defineProperty(container, asset.name, {
 			get : function(){ 
-				return game.cache.getImage(path+asset.name);
-			}
+				return asset;
+			},
+			enumerable: true
 		});
 		
 	},
@@ -62,32 +69,30 @@ global.mt = {
 		group = group || this.game.world;
 		path = path !== "" ? "." + path : path;
 		
-		var object = null;
-		var tmpGroup = null;
-		
 		for(var i = data.length - 1; i > -1; i--){
-			object = data[i];
-			if(object.contents && object.contents.length){
-				tmpGroup = this._addGroup(object, container);
-				
-				if(!container[object.name]){
-					container[object.name] = {
-						get self(){
-							return tmpGroup
-						}
-					};
-				}
-				
-				
-				group.add(tmpGroup);
-				
-				this._loadObjects(object.contents, container[object.name], path + object.name, tmpGroup );
-			}
-			else{
-				this._addObject(object, container, group);
-			}
+			this._add(data[i], container, path, group);
 		}
 		
+	},
+	_add: function(object, container, path, group){
+		
+		if(object.contents){
+			var tmpGroup = this._addGroup(object, container);
+			group.add(tmpGroup);
+			
+			if(!container[object.name]){
+				container[object.name] = {
+					get self(){
+						return tmpGroup
+					}
+				};
+			}
+			
+			this._loadObjects(object.contents, container[object.name], path + object.name, tmpGroup );
+		}
+		else{
+			this._addObject(object, container, group);
+		}
 	},
 	
 	_addGroup: function(object){
@@ -96,18 +101,33 @@ global.mt = {
 		group.x = object.x;
 		group.y = object.y;
 		
+		group.name = object.name;
+		
 		if(object.angle){
 			group.angle = object.angle;
 		}
 		
 		return group;
 	},
-   
+ 
 	_addObject: function(object, container, group){
 		
 		var sp = null;
+		group = group || this.game.world;
 		
-		sp = group.create(object.x, object.y, object.assetPath);
+		sp = group.create(object.x, object.y, object.assetKey);
+		
+		var frameData = game.cache.getFrameData(object.assetKey);
+		
+		if(frameData){
+			var arr = [];
+			for(var i=0; i<frameData.total; i++){
+				arr.push(i);
+			}
+			sp.animations.add("default", arr, (object.fps !== void(0) ? object.fps : 10) , false);
+			sp.frame = object.frame;
+		}
+		
 		
 		if(object.angle){
 			sp.angle = object.angle;
@@ -119,11 +139,15 @@ global.mt = {
 		
 		sp.x = object.x;
 		sp.y = object.y;
+		sp.scale.x = object.scaleX;
+		sp.scale.y = object.scaleY;
+		
 		
 		Object.defineProperty(container, object.name, {
 			get : function(){ 
 				return sp;
-			}
+			},
+			enumerable: true
 		});
 		
 		
