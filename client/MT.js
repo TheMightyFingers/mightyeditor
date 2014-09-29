32414f2b46f4ec4923ef28b036ee191ee0cbadbe
Kaspars Stauzs <stauzs@gmail.com>
2014-09-29 20:20:53 +0300

    physics debug draw

--- client/MT.js
+++ client/MT.js
@@ -8906,8 +8906,8 @@ MT.plugins.MapEditor = MT.extend("core.Emitter").extend("core.BasicPlugin")(
 			if(!this.enabledPhysics){
 				return;
 			}
-			for(var i=0; i<this.objects.length; i++){
-				this.drawPhysicsBody(ctx, this.objects[i]);
+			for(var i=0; i<this.loadedObjects.length; i++){
+				this.drawPhysicsBody(ctx, this.loadedObjects[i]);
 			}
 		},
 		
@@ -8924,7 +8924,7 @@ MT.plugins.MapEditor = MT.extend("core.Emitter").extend("core.BasicPlugin")(
 				}
 			}
 			
-			if(!this.isVisible(obj)){
+			if(!obj.isVisible){
 				return;
 			}
 			if(obj.data.contents){
@@ -8933,12 +8933,12 @@ MT.plugins.MapEditor = MT.extend("core.Emitter").extend("core.BasicPlugin")(
 			
 			var p = obj.data.physics;
 			if(!p || !p.enable){
-				var pp = obj.parent;
+				var pp = obj.object.parent;
 				if(obj.parent == obj.game.world){
 					pp = this.settings.physics;
 				}
 				else{
-					pp = pp.data.physics;
+					pp = pp.magic.data.physics;
 				}
 				if(!pp || !pp.enable){
 					return;
@@ -8948,21 +8948,20 @@ MT.plugins.MapEditor = MT.extend("core.Emitter").extend("core.BasicPlugin")(
 			
 			
 			var alpha = ctx.globalAlpha;
-			var bounds = obj.getBounds();
-			var group = obj.parent;
+			//this.object.updateTransform();
 			
-		
+			var mat = obj.object.worldTransform
 			
-			var x = this.getObjectOffsetX(group);
-			var y = this.getObjectOffsetY(group);
+			var x = mat.tx;
+			var y = mat.ty;
 			
 			ctx.save();
 			
 			ctx.fillStyle = "rgb(100,200,70)";
 			ctx.globalAlpha = 0.2;
 			
-			var w = bounds.width;
-			var h = bounds.height;
+			var w = obj.width;
+			var h = obj.height;
 			
 			if(p.size.width > 0){
 				w = p.size.width * this.scale.x;
@@ -8971,9 +8970,9 @@ MT.plugins.MapEditor = MT.extend("core.Emitter").extend("core.BasicPlugin")(
 				h = p.size.height * this.scale.y;
 			}
 			
+			ctx.setTransform(mat.a, -mat.b, -mat.c, mat.d, mat.tx, mat.ty);
 			
-			
-			ctx.fillRect((bounds.x + p.size.offsetX*this.scale.x) | 0, (bounds.y + p.size.offsetY*this.scale.y) | 0, w, h);
+			ctx.fillRect(-w*obj.object.anchor.x + p.size.offsetX ,-h*obj.object.anchor.y + p.size.offsetY, w, h);
 			ctx.restore();
 		},
 		
