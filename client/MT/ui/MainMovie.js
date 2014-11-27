dd842021ff4698f39b86c41bfae66b1187f49861
Kaspars Stauzs <stauzs@gmail.com>
2014-11-27 12:44:36 +0200

    inert complete

---
+++ client/MT/ui/MainMovie.js
@@ -0,0 +1,26 @@
+MT(
+	MT.ui.MainMovie = function(mm){
+		this.mm = mm;
+
+	},
+	{
+		show: function(){
+			this.tv = new MT.ui.TreeView([{name: "XXX"}], {root: pp.path});
+			this.tv.tree.addClass("ui-keyframes-tree");
+			
+			this.mm.leftPanel.content.el.appendChild(this.tv.tree.el);
+			this.mm.leftPanel.show();
+			this.mm.rightPanel.show();
+			
+			
+			this.mm.leftPanel.style.borderRightStyle = "solid";
+		},
+		
+		hide: function(){
+			this.tv.tree.hide();
+			
+		},
+
+
+	}
+);
