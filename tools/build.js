dde3fe78e86c4d5cd474f174e72698784df5f3ed
Kaspars Stauzs <stauzs@gmail.com>
2015-02-10 12:58:50 +0200

    minify fix

--- tools/build.js
+++ tools/build.js
@@ -131,5 +131,5 @@ for(var i in miscFiles){
 	console.log("minify: ", miscFiles[i]);
 	result = UglifyJS.minify(miscFiles[i]+".js", {outSourceMap: miscFiles[i]+".min.js.map"});
 	fs.writeFile(miscFiles[i]+".min.js", result.code);
-	fs.writeFile(this.filename+".map", result.map);
+	fs.writeFile(miscFiles[i]+".map", result.map);
 }
